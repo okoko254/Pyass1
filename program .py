54376de100d1a92deba222320280f4378bc417a7
Rebecca Okoko <okokorebecca39@gmail.com>
2025-01-04 00:05:18 +0530

    Add files via upload

---
+++ program .py
@@ -0,0 +1,4 @@
+name_input = input("Enter you name: ")
+color_input = input("Enter your favorite color: ")
+
+print("Hey there " + name_input + "!, your favorite color, " + color_input + " is awesome")
